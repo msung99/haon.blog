75b13e648b6927cdc0ac093d7cd196144239c23c
Haon <msung6924@naver.com>
2024-05-12 19:01:27 +0900

    소개문구 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@
 module.exports = {
   siteMetadata: {
     title: `haon.blog`,
-    description: `꾸준히 성장하는 과정속에서, 배움을 공유하는 블로그입니다.`,
+    description: `꾸준히, 배움에 대한 생각을 글로 정제하기 위한 블로그입니다.`,
     author: `Haon`,
     siteUrl: `https://haon.blog/`,
     keywords: [`backend`, `gatsby starter`], 
